980da4607cc7470eaf46f8efd7ecf497d7bcef6e
Malcolm Jackson <mj@deepplanet.com>
2018-08-18 19:46:46 -0700

    completed

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
+  this.first = 1;
+  this.latest = 1;
 };
 
+Queue.prototype.enqueue = function(value){
+  //console.log(this.count);
+  this.storage[this.latest] = value; 
+  this.count ++;
+  this.latest ++;
+},
+Queue.prototype.dequeue = function(){
+  if(this.count > 0){
+    var dequeued = this.storage[this.first];
+   // console.log('dequeued = ', dequeued)
+    delete this.storage[this.first]; 
+    this.count --;
+    this.first++; 
+  }
+  return dequeued;
+},
+Queue.prototype.size = function() {
+  return this.count;
+}  
 
