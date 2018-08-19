f00f01c3f22bb856367252115c9e36448dd98f31
Malcolm Jackson <mj@deepplanet.com>
2018-08-18 19:45:42 -0700

    completed

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,31 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // adds to top of stack
+    storage[size + 1] = value; 
+    size ++;
+   // console.log('my storage object = ', storage); 
+    //console.log('size = ', size);
   };
 
   someInstance.pop = function() {
+    //console.log('length = ', Object.keys(someInstance).length);
+    if(size > 0){
+      // removes top of stack
+      //console.log('my storage object key to delete = ', storage[size]); 
+      var deleted = storage[size];
+      delete storage[size]; 
+      size --;
+      return deleted;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
