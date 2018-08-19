ef0fd3aecbca1df445a268cea9bc41cbb91526bd
Malcolm Jackson <mj@deepplanet.com>
2018-08-18 19:45:01 -0700

    completed

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,40 @@
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    count: 0,
+    storage: {}
+  };
+
+  extend(someInstance,stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
+var extend = function(someInstance, methods){
+  for(var key in methods){
+    someInstance[key] = methods[key];
+  }
+} 
+
+var stackMethods = {
+  push: function(value){
+    console.log(this.count);
+    this.storage[this.count + 1] = value; 
+    this.count++;
+  },
+  pop: function(){
+    if(this.count > 0){
+      // removes top of stack
+      var deleted = this.storage[this.count];
+      delete this.storage[this.count]; 
+      this.count--;
+      return deleted;
+    }
+  }, 
+  size: function() {
+    return this.count;
+  }
+};
 
 
