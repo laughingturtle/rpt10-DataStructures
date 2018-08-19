c15435e9dc5cceafa703c76c5a2d5d010627b6d5
Malcolm Jackson <mj@deepplanet.com>
2018-08-18 19:45:22 -0700

    completed

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,46 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var first = 1;
+  var latest = 1;
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+   // var enqueued = storage[size + 1] = value;
+    //console.log('enqueued = ', enqueued); 
+    storage[latest] = value; 
+    size ++;
+    latest ++;
+   // return enqueued;
   };
 
   someInstance.dequeue = function() {
+    if(size > 0){
+      var dequeued = storage[first];
+     // console.log('dequeued = ', dequeued)
+      delete storage[first]; 
+      size --;
+      first++; 
+    }
+    return dequeued;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
 };
+
+/*
+
+1st 1 
+2nd 2
+3rd 3 
+4th 4
+5th 5
+
+remove 1st
+now 
+*/
