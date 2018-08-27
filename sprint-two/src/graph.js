b10a5457c395264ec4eaad1d866b92a3287dd8b6
Monkey Do <monkeydo@Monkeys-MacBook-Pro.local>
2018-08-27 10:17:31 -0700

    in process

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,53 +1,89 @@
+// this.newGraph.node = new Graph(node);
+
 // option 1
 // {value: 'val', {fromNode: 'val', toNode:'val' }}
-
 // option 2
 // node = [];
 // edges = [];
-
 // option 3
-// {value: 'val', [fromNode, toNode]}, 
+// {value: 'val', [fromNode, toNode]},
 
+// option 4
+// {'NodeA': 'NodeA', edge: {'nodeA':[fromNode, toNode]},
+// {'NodeB': 'NodeB', edge: {'nodeB':[fromNode, toNode]},
+// {'NodeC': 'NodeC', edge: {'nodeD':[fromNode, toNode]},
 
+// one object stores all the nodes for the Graph
+// another object that stores all the edges of the graph
+// {
+//   Node1: " ",
+//   Node2: " ",
+//   Node3: " "
+// }
+// the important thing is that you get the constant time lookup
+
+// [fromNode, toNode]}
+// node value {nodeA: "nodeA"},
+// node edge {nodeA:"nodeB"},
+
+// Theres an array in here somewhere?
+//forEach node
 
 // Instantiate a new graph
-var Graph = function() {
+var Graph = function(){
     this.newGraph = {};
-    this.vertice = null; // this is the node
-    this.edges = null; // this is the line, or edge, or pointer
-
-   // this.newGraph.node = [];
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  //this.storage = node;
-  this.newGraph[node] = {edges:{}};
-  console.log(this.newGraph);
+    this.newGraph[node] = new Graph(node);
+    //console.log(this.newGraph);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  if(this.newGraph[node]){
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+   //removeEdge(this.newGraph[node]);
+   delete this.newGraph[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  if (this.newGraph[fromNode][toNode]){
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.newGraph[fromNode][toNode] = true;
+  this.newGraph[toNode][fromNode] = true;
+  console.log(this.newGraph);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this.newGraph[fromNode][toNode];
+  delete this.newGraph[toNode][fromNode];
+  this.removeNode(fromNode);
+
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for(var i in key){
+
+  }
 };
 
 /*
